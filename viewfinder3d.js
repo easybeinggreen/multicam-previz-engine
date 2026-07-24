1738eae25fd0803e710bbf40c729879e9eccca52
Easybeinggreen <green.paul.p@gmail.com>
2026-07-25 07:04:48 +1000

    Update model file name for loading Brian

--- viewfinder3d.js
+++ viewfinder3d.js
@@ -92,7 +92,7 @@ async function init3D() {
   // --- Build static Brian model (skinning stripped) ---
   async function loadBrianModel() {
     const loader = new GLTFLoader();
-    const url = './brian.glb';
+    const url = './Brian_A_260724.glb';
     console.log('🔄 Loading Brian from:', url);
     try {
       const gltf = await new Promise((resolve, reject) => {
